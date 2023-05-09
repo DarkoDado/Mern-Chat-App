5ee52b5e77fe533fbd5b550f8a2cf84ceec40664
Darko Pantic <gusar97@gmail.com>
2023-05-09 23:52:09 +0200

    make some changes on login api

--- api/index.js
+++ api/index.js
@@ -38,14 +38,18 @@ app.get('/profile', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const foundUser = await User.findOne({ username })
-    if(foundUser) {
-        const passOk = bcrypt.compareSync(password, foundUser.password)
-        if(passOk) {
-            jwt.sign()
+    const foundUser = await User.findOne({ username });
+    if (foundUser) {
+        const passOk = bcrypt.compareSync(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+                res.cookie('token', token, { sameSite: 'none', secure: true }).json({
+                    id: foundUser._id,
+                });
+            });
         }
     }
-})
+});
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
