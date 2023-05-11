3b393dc7daf1cfc271872cb734ec320c47847e26
Darko Pantic <gusar97@gmail.com>
2023-05-11 22:59:53 +0200

    Added user avatars, and colors

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react'
 import { BsFillSendPlusFill } from 'react-icons/bs'
 import { HiChatAlt } from 'react-icons/hi'
+import Avatar from './Avatar'
 export default function Chat() {
   const [ws, setWs] = useState(null)
   const [onlinePeople, setOnlinePeople] = useState({})
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:4000')
     setWs(ws)
+    function handleMessage(e) {
+      const messageData = JSON.parse(e.data)
+      if ('online' in messageData) {
+        showOnlinePeople(messageData.online)
+      }
+    }
     ws.addEventListener('message', handleMessage)
-  }, [])
+  }, [ws])
   function showOnlinePeople(peopleArray) {
     const people = {}
     peopleArray.forEach(({ userId, username }) => {
@@ -16,15 +23,10 @@ export default function Chat() {
     })
     setOnlinePeople(people)
   }
-  function handleMessage(e) {
-    const messageData = JSON.parse(e.data)
-    if ('online' in messageData) {
-      showOnlinePeople(messageData.online)
-    }
-  }
+
   return (
     <div className="flex h-screen">
-      <div className="bg-blue-200 w-2/3 p-2 flex flex-col">
+      <div className="bg-blue-200 w-3/4 p-2 flex flex-col">
         <div className="flex-grow">Messages with selected person</div>
         <div className="flex gap-2 mx-3">
           <input
@@ -34,20 +36,24 @@ export default function Chat() {
           />
           <button className="bg-blue-500 px-3 py-2 text-white rounded-sm">
             <BsFillSendPlusFill
-              style={{ fontSize: '26px' }}
+              style={{ fontSize: '32px' }}
               className="transition-all hover:text-blue-200"
             />
           </button>
         </div>
       </div>
-      <div className="bg-white w-1/3 pl-4 pt-4">
-        <div className="text-blue-500 font-bold flex gap-1 items-center mb-3">
-          <HiChatAlt size={34}/>
+      <div className="bg-white w-1/8 pl-4 pt-4">
+        <div className="text-blue-500 font-bold flex gap-1 items-center mb-3 text-lg">
+          <HiChatAlt size={34} />
           MernChat
         </div>
         {Object.keys(onlinePeople).map((userId) => (
-          <div key={userId} className="border-b border-gray-100 py-2">
-            {onlinePeople[userId]}
+          <div
+            key={userId}
+            className="border-b border-gray-100 py-2 cursor-pointer flex items-center gap-2"
+          >
+            <Avatar username={onlinePeople[userId]} userId={userId} />
+            <span className="text-gray-700">{onlinePeople[userId]}</span>
           </div>
         ))}
       </div>
